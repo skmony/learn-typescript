64a21330446a8f90bceb824da177f831025d8328
Shashikant Mony <shashikantmony@gmail.com>
2020-04-08 03:26:53 +0530

    initial commit

---
+++ starting-out/app.js
@@ -0,0 +1,7 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var a = 27;
+var b = 78;
+var sum = add(a, b);
+console.log(sum);
---
+++ starting-out/app.ts
@@ -0,0 +1,7 @@
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+var a = 27;
+var b = 78;
+var sum = add(a, b);
+console.log(sum);
